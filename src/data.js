6e2a5685aaba4fe75e6900589e6faf93b683f17c
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-06-20 00:49:36 -0400

    Done.

--- src/data.js
+++ src/data.js
@@ -3,57 +3,57 @@ const movieData = [
     title: 'Choux and Maru go to Istanbul',
     IMDBRating: 3,
     genres: ['cats', 'adventure', 'romance'],
-    poster: 'choux-maru-istanbul'
+    poster: 'choux-maru-istanbul',
   },
   {
     title: 'Choux and Maru',
     IMDBRating: 4,
     genres: ['cats', 'drama', 'romance'],
-    poster: 'choux-maru-part-1'
+    poster: 'choux-maru-part-1',
   },
   {
     title: 'ChromeBoi',
     IMDBRating: 5,
     genres: ['thriller', 'romance'],
-    poster: 'chromeboi'
+    poster: 'chromeboi',
   },
-  
+
   {
     title: 'Escape from VIM',
     IMDBRating: 0,
     genres: ['an awful movie'],
-    poster: 'escape-from-vim'
+    poster: 'escape-from-vim',
   },
   {
     title: 'GoldenEye',
     IMDBRating: 4,
     genres: ['mystery', 'thriller', 'crime'],
-    poster: 'goldeneye'
+    poster: 'goldeneye',
   },
   {
     title: 'Handsome Boy Modeling Club',
     IMDBRating: 2,
     genres: ['coming of age', 'romance'],
-    poster: 'handsome-boy'
+    poster: 'handsome-boy',
   },
   {
     title: "Maru's Spinoff: Trapped in the Sheets",
     IMDBRating: 5,
     genres: ['Adventure'],
-    poster: 'marus-spinoff'
+    poster: 'marus-spinoff',
   },
   {
     title: 'Terrance: King of the Rats',
     IMDBRating: 1,
     genres: ['drama'],
-    poster: 'terrance-king'
+    poster: 'terrance-king',
   },
   {
     title: 'The Trash Man',
     IMDBRating: 5,
     genres: ['crime', 'period piece'],
-    poster: 'the-trash-man'
+    poster: 'the-trash-man',
   },
-]
+];
 
-export default movieData
+export default movieData;
